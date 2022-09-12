a3fc4e4c8b8e8d50824c13b4fce6bda606d974bf
MinhPho <quangminhpho@hotmail.com>
2022-09-12 07:48:19 +0200

    added support checkout branch and commit for git2go solution & example config.yaml

--- example/git2go/git2go.go
+++ example/git2go/git2go.go
@@ -10,7 +10,20 @@ import (
 )
 import "os/exec"
 
-func commitExist(repoDir, commitId string) bool {
+func inputIsBranch(repoDir, commitId string) bool {
+	cmd := exec.Command("git", "rev-parse", "--verify", fmt.Sprintf("refs/remotes/origin/%s", viper.GetString("branch")))
+	cmd.Dir = repoDir
+
+	if err := cmd.Run(); err != nil {
+		if _, ok := err.(*exec.ExitError); ok {
+			// Git exit code 128
+			return false
+		}
+	}
+	return true
+}
+
+func inputIsCommitId(repoDir, commitId string) bool {
 	cmd := exec.Command("git", "rev-list", fmt.Sprintf("HEAD..%s", viper.GetString("branch")))
 	cmd.Dir = repoDir
 
@@ -34,6 +47,7 @@ func main() {
 
 	dir, _ := ioutil.TempDir("", "temp_dir")
 	fmt.Println(dir)
+
 	options := &git2go.CloneOptions{
 		CheckoutBranch: "main",
 	}
@@ -61,8 +75,7 @@ func main() {
 	// fmt.Println(commit)
 	// repo.CheckoutIndex()
 
-	// TODO: differenciate between branch and commit
-	if commitExist(dir, viper.GetString("branch")) {
+	if inputIsCommitId(dir, viper.GetString("branch")) {
 		// Poor man solution to checkout to specific commit id :(
 		cmd := exec.Command("git", "checkout", viper.GetString("branch"))
 		cmd.Dir = dir
@@ -70,6 +83,15 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
+	} else if inputIsBranch(dir, viper.GetString("branch")) {
+		cmd := exec.Command("git", "checkout", viper.GetString("branch"))
+		cmd.Dir = dir
+		err = cmd.Run()
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		panic("input is neither a valid branch nor commit id")
 	}
 }
 
